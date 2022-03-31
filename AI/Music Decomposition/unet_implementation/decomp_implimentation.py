e157292bf3a052fefa6238b580731d4dfc93a693
VikramRamanathan <42689333+VikramRamanathan@users.noreply.github.com>
2022-03-31 13:35:04 +0800

    Implementation done somewhat?

--- AI/Music Decomposition/unet_implementation/decomp_implimentation.py
+++ AI/Music Decomposition/unet_implementation/decomp_implimentation.py
@@ -2,7 +2,6 @@ import tensorflow
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers, Model
-import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import numpy as np
 from functools import partial
@@ -17,19 +16,51 @@ class Unet(Model):
         self.layer_1 = partial(self.downsample, n_filters=16, dropout=True)
         self.layer_2 = partial(self.downsample, n_filters=32, dropout=True)
         self.layer_3 = partial(self.downsample, n_filters=64, dropout=True)
-        self.layer_4 = partial(self.downsample, n_filters=128, dropout=True)
-        self.layer_5 = partial(self.downsample, n_filters=256, dropout=True)
-        self.layer_6 = partial(self.downsample, n_filters=512, dropout=True)
+        self.layer_4 = partial(self.downsample, n_filters=128, dropout=False)
+        self.layer_5 = partial(self.downsample, n_filters=256, dropout=False)
+        self.layer_6 = partial(self.downsample, n_filters=512, dropout=False)
 
         self.layer_7 = layers.Conv2D(1024, (4, 4), output_padding=1, activation="relu",
-                                kernel_initializer="he_normal")
+                                     kernel_initializer="he_normal")
 
-        self.layer_8 = partial(self.upsample, n_filters=512, dropout=True)
-        self.layer_8 = partial(self.upsample, n_filters=256, dropout=True)
-        self.layer_8 = partial(self.upsample, n_filters=128, dropout=True)
-        self.layer_8 = partial(self.upsample, n_filters=64, dropout=True)
-        self.layer_8 = partial(self.upsample, n_filters=32, dropout=True)
-        self.layer_8 = partial(self.upsample, n_filters=16, dropout=True)
+        self.layer_8 = partial(self.upsample, n_filters=512, dropout=False)
+        self.layer_9 = partial(self.upsample, n_filters=256, dropout=False)
+        self.layer_10 = partial(self.upsample, n_filters=128, dropout=False)
+        self.layer_11 = partial(self.upsample, n_filters=64, dropout=True)
+        self.layer_12 = partial(self.upsample, n_filters=32, dropout=True)
+        self.layer_13 = partial(self.upsample, n_filters=16, dropout=True)
+
+    def call(self, x):
+        x = self.layer_1(x, self.training)
+        print(x.shape)
+        x = self.layer_2(x, self.training)
+        print(x.shape)
+        x = self.layer_3(x, self.training)
+        print(x.shape)
+        x = self.layer_4(x, self.training)
+        print(x.shape)
+        x = self.layer_5(x, self.training)
+        print(x.shape)
+        x = self.layer_6(x, self.training)
+        print(x.shape)
+
+        x = self.layer_7(x, self.training)
+        print(x.shape)
+
+        x = self.layer_8(x, self.training)
+        print(x.shape)
+        x = self.layer_9(x, self.training)
+        print(x.shape)
+        x = self.layer_10(x, self.training)
+        print(x.shape)
+        x = self.layer_11(x, self.training)
+        print(x.shape)
+        x = self.layer_12(x, self.training)
+        print(x.shape)
+        x = self.layer_13(x, self.training)
+        print(x.shape)
+        return x
+
 
     def downsample(self, x, n_filters, dropout=False):
         pass_on = layers.LeakyReLU()(x)
@@ -38,12 +69,12 @@ class Unet(Model):
 
         pass_on = layers.BatchNormalization(pass_on, self.training)
         if dropout:
-            pass_on = layers.Dropout(0.5)(pass_on)
+            pass_on = layers.Dropout(0.4)(pass_on)
         return pass_on
 
     def upsample(self, x, orig_features, n_filters, dropout=False):
         upsampled = layers.LeakyReLU()(x)
-        upsampled = layers.Conv2DTranspose(n_filters, (3, 3), output_padding=1, strides=1
+        upsampled = layers.Conv2DTranspose(n_filters, (3, 3), output_padding=1, strides=1,
                                            kernel_initializer="he_normal")(upsampled)
         upsampled = layers.BatchNormalization(upsampled, self.training)
         upsampled = layers.UpSampling2D(2)(upsampled)
